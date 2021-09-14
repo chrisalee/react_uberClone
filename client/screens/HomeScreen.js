735f966ecc755fdf3fb0ec39b59e04c53f1ff602
Chris Lee <christopher.lee222@yahoo.com>
2021-09-14 17:20:47 -0500

    store navReducer homescreen navoptions

---
+++ client/screens/HomeScreen.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View, Image } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import NavOptions from "../components/NavOptions";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        {/* <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        /> */}
+        <Text style={[tw`pb-10`, styles.text]}>UberClone</Text>
+        <NavOptions />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  text: {
+    color: "#000000",
+    fontSize: "2.5em",
+    fontWeight: "800"
+  },
+});
